7e56b14a6dce610984fa5ec1f8d6f30b2cfe2dbf
jnblanchet <jean-nicola@wajam.com>
2013-07-29 09:14:39 -0400

    first push

---
+++ spkr-core/src/main/scala/com/wajam/spkr/mry/model/Feed.scala
@@ -0,0 +1,35 @@
+package com.wajam.spkr.mry.model
+
+/**
+ * Feed model definition.
+ * The feed contains all the message that a member should be able to read.
+ */
+
+object Feed extends Model {
+  // Key 1 from parent table, allows sharding on same node. Refers to the member that reads the messages on his feed.
+  val username = "username"
+  // Key 2, a unique identifier for each
+  val feedId = "feed_id"
+  // The id of the member who wrote the message
+  val subscriptionUsername = "subscription_username"
+  // The name that should be displayed next to the message
+  val subscriptionDisplayName = "subscription_display_name"
+  // Message content is duplicated on the local shard (same shard thanks to the username key).
+  val content = "message_content"
+
+  val id = feedId
+  val definition = Map(
+    username -> PropertyType.String,
+    feedId -> PropertyType.Number,
+    subscriptionUsername -> PropertyType.String,
+    subscriptionDisplayName -> PropertyType.String,
+    content -> PropertyType.String
+  )
+
+  def defaultValues = Map[String, String](
+    username -> "",
+    subscriptionUsername -> "",
+    subscriptionDisplayName -> "",
+    content -> ""
+  )
+}
---
+++ spkr-core/src/main/scala/com/wajam/spkr/mry/model/Subscription.scala
@@ -0,0 +1,28 @@
+package com.wajam.spkr.mry.model
+
+/**
+ * Subscription model definition
+ * Represents a unidirectional link between two members. Subscriptions are only used to populate other member's
+ * subscribers through percolation.
+ */
+
+object Subscription extends Model {
+  val username = "username"
+  val subscriptionId = "subscription_id"
+  val subscriptionUsername = "subscription_username"
+  val subscriptionDisplayName = "subscription_display_name"
+
+  val id = subscriptionId
+  val definition = Map(
+    subscriptionId -> PropertyType.Number,
+    username -> PropertyType.String,
+    subscriptionUsername -> PropertyType.String,
+    subscriptionDisplayName -> PropertyType.String
+  )
+
+  def defaultValues = Map[String, String](
+    username -> "",
+    subscriptionUsername -> "",
+    subscriptionDisplayName -> ""
+  )
+}
